35f3210c651b2033e70e0037c0d71d6eaa4cf301
Priyansh <pradadiya667@gmail.com>
2019-11-02 15:00:27 -0400

    auth.js had an error of request parameter. POST request was using router.get which is changes to router.post

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
 // @route       POST api/auth
 // @desc        Auth user & get token
 // @access      Public
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
   res.send('Log in user');
 });
 
