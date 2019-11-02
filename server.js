7762a30d6cda20a36ec62b8007f8c7ede67ad540
Priyansh <pradadiya667@gmail.com>
2019-11-02 13:50:08 -0400

    created an / route to test postman with json object

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json({ msg: 'Welcome to ContactKeeper API...' });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
