589089b641ff32f3f8169abe1ab13977c7616e51
Priyansh <pradadiya667@gmail.com>
2019-11-03 13:15:08 -0500

    Modified users route - checked whether user exists in database if exsists throw mgs else bcryt the password with using salt. - store to user instance of User Model and add to database.

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,7 @@ const User = require('../models/User');
 // @access      Public
 router.post(
   '/',
+  // Validation
   [
     check('name', 'Please add name')
       .not()
@@ -19,13 +20,36 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ error: errors.array() });
     }
 
-    res.send('passed');
+    // Destructure req.body
+    const { name, eamil, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcryt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User Saved');
+    } catch (err) {}
   }
 );
 
