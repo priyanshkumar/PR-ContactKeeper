c7d6fb77003ecb4a2cb542638ef4186a0f62f884
Priyansh <pradadiya667@gmail.com>
2019-11-03 16:52:37 -0500

    Modified user route - Added functionality that issues jwt token as soon as user register

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-
+const config = require('config');
 // @route       POST api/users
 // @desc        Register a user
 // @access      Public
@@ -27,7 +28,7 @@ router.post(
     }
 
     // Destructure req.body
-    const { name, eamil, password } = req.body;
+    const { name, email, password } = req.body;
 
     try {
       let user = await User.findOne({ email });
@@ -42,14 +43,33 @@ router.post(
         password
       });
 
-      const salt = await bcryt.genSalt(10);
+      const salt = await bcrypt.genSalt(10);
 
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
 
-      res.send('User Saved');
-    } catch (err) {}
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error');
+    }
   }
 );
 
