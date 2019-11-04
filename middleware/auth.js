df87e12f79b4bcdc9a82fae36c36ca7e6aa0cacb
Priyansh <pradadiya667@gmail.com>
2019-11-04 12:26:43 -0500

    Modified contacts.js route
    modified PUT route having private access which will update the contact
    - passed through middleware
    - build an updated req.body object
    - check for the contact exist or not
    - if not 404 error
    - if found check whether it matches the user id or not
    - if not matche user id error 401
    - if matches then update the dtabase for that contact
    - send back contact in json form

--- middleware/auth.js
+++ middleware/auth.js
@@ -16,6 +16,6 @@ module.exports = function(req, res, next) {
     req.user = decode.user;
     next();
   } catch (err) {
-    res, status(401).json({ msg: 'Token is not valid' });
+    res.status(401).json({ msg: 'Token is not valid' });
   }
 };
