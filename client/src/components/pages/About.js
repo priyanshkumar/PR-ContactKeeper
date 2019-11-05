76e782eb5064821638c939055307f874d3f5ee96
Priyansh <pradadiya667@gmail.com>
2019-11-05 14:37:41 -0500

    Modified navabr.js by adding following LINKS  - Home 	/ - About		/about

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -7,7 +7,9 @@ const About = () => {
       <p className="my-1">
         This is a Full Stack react App for keeping contacts.
       </p>
-      <p className="bg-dark p"><strong>Version: </strong>1.0.0</p>
+      <p className="bg-dark p">
+        <strong>Version: </strong>1.0.0
+      </p>
     </div>
   );
 };
