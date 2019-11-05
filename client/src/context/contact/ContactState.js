6ad670f714d89a4f2d11df05d45942ec1210bee2
Priyansh <pradadiya667@gmail.com>
2019-11-05 18:17:48 -0500

    Modified ContactState.js with intialState that stores contacts. - For now contacts array have predefined data for test purpose. also initialised reducer to use ContactReducer and intialState

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -11,3 +11,35 @@ import {
   FILTER_CONTACTS,
   CLEAR_FILTER
 } from '../types';
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        type: 'professional',
+        id: 1,
+        name: 'Pooja Vyas',
+        email: 'poojavyas@gmail.com',
+        phone: '111-111-1111'
+      },
+      {
+        type: 'personal',
+        id: 2,
+        name: 'RD',
+        email: 'Priyansh@gmail.com',
+        phone: '7057337875'
+      },
+      {
+        type: 'personal',
+        id: 3,
+        name: 'PR',
+        email: 'Priyansh@gmail.com',
+        phone: '7057337875'
+      }
+    ]
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Add Contact
+};
