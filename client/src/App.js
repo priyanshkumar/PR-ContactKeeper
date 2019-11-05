96cfc83e945c36ce690f640d354b59e4bfbf2c2f
Priyansh <pradadiya667@gmail.com>
2019-11-05 14:29:14 -0500

    Created Home.js And About.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 import './App.css';
 
 const App = () => {
@@ -9,10 +11,10 @@ const App = () => {
       <Fragment>
         <Navbar />
         <div className="container">
-          <switch>
+          <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
-          </switch>
+          </Switch>
         </div>
       </Fragment>
     </Router>
