6ce1894f6bb1a6121a0f50f61bc1af7c1f362eb5
Priyansh <pradadiya667@gmail.com>
2019-11-06 11:37:01 -0500

    Modified type to have first letter uppercase with help of charAt and slice

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,7 +1,22 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
-const ContactItem = () => {
-  return <Fragment></Fragment>;
+const ContactItem = ({ contact }) => {
+  const { name, email, phone, type, id } = contact;
+  return (
+    <div className="card bg-light">
+      <h3 className="text-primary text-left">
+        {name}{' '}
+        <span
+          className={
+            'badge ' +
+            (type === 'professional' ? 'badge-success' : 'badge-primary')
+          }
+        >
+          {type.charAt(0).toUpperCase() + type.slice(1)}
+        </span>
+      </h3>
+    </div>
+  );
 };
 
 export default ContactItem;
